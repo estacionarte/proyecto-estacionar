e785a969a7830f14cb685976894cc903f82fae8f
soylatrufa <marianoalvareztt@gmail.com>
2017-09-13 13:58:40 -0300

    underconstruction page

--- HTML/underconstruction.php
+++ HTML/underconstruction.php
@@ -6,11 +6,13 @@
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
-      <img class="construccion" src="images/const.gif">
-
+    <div class="bg">
+      <div class="constuction-txt">
         <h2>DISCULPE LAS MOLESTIAS</h2>
         <h3>Estamos trabajando para usted <br>Atte. El equipo de <a href="faqs.php">PROYECTO ESTACIONAR</a></h3>
         <a href="index.php" class="volver-home">VOLVER AL HOME</a>
+      </div>
+    </div>
 
   </body>
 </html>
