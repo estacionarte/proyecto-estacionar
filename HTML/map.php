3124af2f018e55b5cf17e97f5b6fd0fb3350f816
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2017-09-14 16:17:04 -0300

    pruebas en map.php

--- HTML/map.php
+++ HTML/map.php
@@ -14,15 +14,46 @@
   <div class="container">
     <?php require_once('header.php'); ?>
 
-    <div class="bodies-main-content">
+    <div class="map-main-content">
 
       <hr>
 
-      <h1></h1>
+      <!-- <h1>Elige un lugar donde estacionar</h1> -->
 
-      <section class="signin">
+      <section class="map-estacionamientos">
+
+        <div id="map"></div>
+
+        <article class="cochera">
+          <div class="cochera-numero">
+            1
+          </div>
+          <div class="cochera-info">
+            <h3>Av. Independencia 900-1000</h3>
+            <p>Entre Bernardo de Irigoyen y Tacuarí</p>
+            <p>Disponible: 14:00 - 18:00</p>
+          </div>
+          <div class="cochera-precio">
+            $50
+          </div>
+        </article>
+
+        <article class="cochera">
+          <div class="cochera-numero">
+            2
+          </div>
+          <div class="cochera-info">
+            <h3>Tacuarī 800-900</h3>
+            <p>Entre Av. Independencia y Estados Unidos</p>
+            <p>Disponible: 09:00 - 20:00</p>
+          </div>
+          <div class="cochera-precio">
+            $50
+          </div>
+        </article>
 
       </section>
+
     </div>
     <?php require_once('footer.php'); ?>
   </div>
