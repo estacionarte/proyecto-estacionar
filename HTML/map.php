591556891f6b17066ee19bbf0314fd37b591a883
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2017-09-14 12:21:57 -0300

    cree un archivo map.php sobre el que voy a hacer los tests para la pagina donde se muestran las cocheras

---
+++ HTML/map.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>EstacionARte</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+  <div class="container">
+    <?php require_once('header.php'); ?>
+
+    <div class="bodies-main-content">
+
+      <hr>
+
+      <h1></h1>
+
+      <section class="signin">
+
+      </section>
+    </div>
+    <?php require_once('footer.php'); ?>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="js/menu.js"></script>
+</body>
+</html>
