ad67c62947c47b43388d1914e4b07b6733525939
soylatrufa <marianoalvareztt@gmail.com>
2017-09-16 22:55:59 -0300

    trabajé en la pagina de usuario, header de usuario  y en la profile-nav

--- HTML/header-profile.php
+++ HTML/header-profile.php
@@ -1,35 +1,29 @@
 <header class="main-header">
-        <a href="index.php"><h1>EstacionARte</h1></a>
-        <a href="#"><img src="icons/avatar.png" alt="avatar" class="avatar"></a>
-        <nav class="profile-nav">
-          <ul>
-            <li><a href="profile-edit.php" class="">Editar Perfil</a></li>
-            <li><a href="#" class="">Configuración de mi cuenta</a></li>
-            <li class="#"><a href="faqs.php" class="faq-btn">Ayuda</a></li>
-            <li class=""><a href="#" class="">Salir</a></li>
-          </ul>
-        </nav>
-        <!-- <div class="modal-wrapper" id="popup-iniciar">
-     <div class="popup-contenedor">
-        <a class="popup-cerrar" href="#">X</a>
-        <h2 class="title-signin">Iniciar sesión</h2>
-        <section class="signin-popup">
-          <div class="form-generico">
-            <form action="" method="post">
-              <input type="email" name="email" placeholder="Email"/>
-              <input type="password" name="password" placeholder="Contraseña"/>
-              <input type="checkbox" name="recordarme" value="recordarme" id="recordarme"><label for="recordarme">Recordarme</label>
-              <input type="submit" name="" value="INICIAR SESIÓN">
-            </form>
-          </div>
-          <a href="forgot-password.php">¿Olvidaste tu e-mail o contraseña?</a>
-          <a href="signup.php">¿Aún no estás registrado?</a>
-          <div class="login-separador">
-            <span>O</span>
-          </div>
-          <a href="#" class="facebook-login-button">Iniciar sesion con Facebook</a>
-          <a href="#" class="google-login-button">Iniciar sesión con Google</a>
-        </section>
-     </div>
-  </div> -->
+    <a href="index.php"><h1>EstacionARte</h1></a>
+
+    <div class="main-search">
+      <form class="main-form-search" action="" method="get">
+        <input type="text" name="buscar-texto" placeholder="Buscar cocheras">
+        <input type="submit" name="buscar-submit" value="">
+      </form>
+    </div>
+
+    <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img src="icons/close-profile-nav2.png" alt=""></a>
+  <a href="profile-edit.php">Editar perfil</a>
+  <a href="#">Configuración de mi cuenta</a>
+  <a href="#">Ayuda</a>
+  <a href="#">Salir</a>
+</div>
+
+<span style="font-size:30px;cursor:pointer" onclick="openNav()"><img src="icons/avatar.png" alt="avatar" class="avatar"></span>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";}
+</script>
+
 </header>
--- HTML/profile-edit.php
+++ HTML/profile-edit.php
@@ -15,9 +15,19 @@
         <a href="#">Reputación</a>
       </nav>
 
+      <div class="">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </section>
   </div>
   <?php require_once('footer.php'); ?>
 </div>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<!-- <script src="js/menu.js"></script> -->
+<script>
+  $('.avatar').click(function(){
+    $('.profile-nav').slideToggle('open-nav');
+  });
+</script>
   </body>
 </html>
