3a182233f7a1d3ed258032640d99d0ac06732fdc
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2017-10-23 14:31:43 -0300

    listo el maquetado de carga de cocheras, solo falta una pagina final de resumen y confirmacion

--- HTML/upload-estacionamiento-4precios.php
+++ HTML/upload-estacionamiento-4precios.php
@@ -29,28 +29,34 @@
 
             <label for="" class="upload-label-titulo">¿Cuál va a ser el precio por minuto de tu espacio?</label>
 
-            <div class="upload-div-precioPorMinuto">
+            <div class="upload-div-precio">
+              <label for="" class="upload-label-precioPorMinutoSigno">$</label>
               <input type="number" placeholder="0,75" name="precioPorMinuto" class="upload-input-precioPorMinuto" min="0,1" max="2">
               <label for="" class="upload-label-precioPorMinuto">por minuto</label>
             </div>
 
-            <label for="" class="upload-label-tiempoMinimoyMax">Máximo</label>
-            <select name="medidaDeTiempo" class="upload-select-tiempoMinimoyMax">
-              <option value="Minutos">minutos</option>
-              <option value="Horas">horas</option>
-              <option value="Dias">días</option>
-            </select>
-            <input type="number" placeholder="15" name="tiempoMinimoyMax" class="upload-input-tiempoMinimoyMax" min="0" max="10000">
+            <label for="" class="upload-label-titulo">¿Cuál va a ser el descuento para alquileres prolongados?</label>
 
-            <label for="" class="upload-label-titulo">¿Cuánta anticipación se necesita para la reserva?</label>
+            <div class="upload-div-precio">
+              <input type="number" placeholder="20" name="descuentoPorMinuto" class="upload-input-descuentoPorMinuto" min="0" max="90">
+              <label for="" class="upload-label-descuentoPorMinutoPorcentaje">%</label>
+              <label for="" class="upload-label-descuentoPorMinuto">por minuto a partir de la hora</label>
+            </div>
+            <p class="upload-p-descuentoPorMinuto">Precio por hora con descuento: $36</p>
 
-            <label for="" class="upload-label-tiempoMinimoyMax">Anticipación</label>
-            <select name="medidaDeTiempo" class="upload-select-tiempoMinimoyMax">
-              <option value="Minutos">minutos</option>
-              <option value="Horas">horas</option>
-              <option value="Dias">días</option>
-            </select>
-            <input type="number" placeholder="15" name="tiempoMinimoyMax" class="upload-input-tiempoMinimoyMax" min="0" max="10000">
+            <div class="upload-div-precio">
+              <input type="number" placeholder="35" name="descuentoPorMinuto" class="upload-input-descuentoPorMinuto" min="0" max="90">
+              <label for="" class="upload-label-descuentoPorMinutoPorcentaje">%</label>
+              <label for="" class="upload-label-descuentoPorMinuto">por minuto a partir de 6 horas</label>
+            </div>
+            <p class="upload-p-descuentoPorMinuto">Precio cada 6 horas con descuento: $175,50</p>
+
+            <div class="upload-div-precio">
+              <input type="number" placeholder="70" name="descuentoPorMinuto" class="upload-input-descuentoPorMinuto" min="0" max="90">
+              <label for="" class="upload-label-descuentoPorMinutoPorcentaje">%</label>
+              <label for="" class="upload-label-descuentoPorMinuto">por minuto a partir del día</label>
+            </div>
+            <p class="upload-p-descuentoPorMinuto">Precio por día con descuento: $324</p>
 
             <input type="submit" name="boton-submit" value="&#8249; Volver" class="upload-button-volver">
             <input type="submit" name="boton-submit" value="SIGUIENTE" class="upload-button-submit">
