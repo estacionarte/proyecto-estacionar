8006e8028d73c5a86166605391f708b8d5daa737
soylatrufa <marianoalvareztt@gmail.com>
2017-11-04 21:23:07 -0300

    errores

--- HTML/soporte.php
+++ HTML/soporte.php
@@ -7,10 +7,6 @@ require_once("clases/dbMySQL.php");
 
 $auth = new Authenticator();
 $validator = new Validator();
-<<<<<<< HEAD
-// $db = new DBJSON();
-=======
->>>>>>> a5d4641323cec691814f361f8c54f5991b16e133
 $db = new DBMySql();
 
 ?>
