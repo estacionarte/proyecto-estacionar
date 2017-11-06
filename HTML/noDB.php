4c74043d2207ddd7f7565613565f65411d4ff34b
PabloMerener <pablo.merener@gmail.com>
2017-11-06 00:07:38 -0300

    sin el header pero funca en todos los SO

--- HTML/noDB.php
+++ HTML/noDB.php
@@ -4,7 +4,7 @@
   require_once('head.php');?>
   <body>
     <div class="home-container">
-      <?php require_once('header.php');?>
+      <?php //require_once('header.php');?>
       <!-- MIGRATE TO SQL -->
       <?php require_once('migrateToSql.php'); ?>
       <div class="clear"></div>
