9632b5fa2e784113e678d22fb7357dfa7f2a3fec
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2017-12-11 16:06:10 -0300

    search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,42 @@
         <input type="submit" name="BUSCAR" value="BUSCAR">
       </form>
     </div>
-    
+
+    <div class="form-generico">
+      <form class="search-espacios-form" action="" method="get">
+        <div class="" id="dir_y_vehiculo">
+          <input type="search" name="search-espacios-input-direccion" placeholder="¿Dónde querés estacionar?">
+          <select name="search-espacios-vehiculo">
+            <option value="">Vehículo</option>
+            <option value="Auto">Auto</option>
+            <option value="Moto">Moto</option>
+            <option value="Bicicleta">Bicicleta</option>
+          </select>
+        </div>
+        <div class="search-horario">
+          <h5>Llegada</h5>
+          <input type="date" name="" value="">
+          <select name="search-espacios-hora-comienzo" class="search-espacios-hora">
+            <option value=""></option>
+          </select>
+          <select name="search-espacios-minuto-comienzo" class="search-espacios-minuto">
+          </select>
+        </div>
+        <div class="search-horario">
+          <h5>Partida</h5>
+          <input type="date" name="" value="">
+          <select name="search-espacios-hora-fin" class="search-espacios-hora">
+
+          </select>
+          <select name="search-espacios-hora-fin" class="search-espacios-minuto">
+
+          </select>
+        </div>
+        <button type="submit" name="search-espacios-submit"><i class="fa fa-search"></i></button>
+        <input type="submit" name="BUSCAR" value="BUSCAR">
+      </form>
+    </div>
+
   </article>
 </section>
 
