b58efbb700883ffbe345dfa8bb8075711e052ca9
soylatrufa <marianoalvareztt@gmail.com>
2017-12-20 16:14:28 -0300

    view y form de espacio

--- resources/views/espacio.blade.php
+++ resources/views/espacio.blade.php
@@ -15,7 +15,14 @@
         {{-- {{ dd($espacio->fotoPortada()) }} --}}
 
         <h2>{{ $espacio->direccion }}</h2>
+        <h4> {{ $espacio->pais }} , {{ $espacio->provincia }} </h4>
+
+        <hr class="estadia-linea">
+
         <h3 style="margin-top:0px; font-size:1.1em;">{{ $espacio->tipoEspacio }}</h3>
+        <h3 style="margin-top:0px; font-size:1.1em;">Estadía con Auto $ {{ $espacio->precioAutosMinuto }}  por minuto</h3>
+        <h3 style="margin-top:0px; font-size:1.1em;">Estadía con Moto $ {{ $espacio->precioMotosMinuto }}  por minuto</h3>
+        <h3 style="margin-top:0px; font-size:1.1em;">Estadía con Bici $ {{ $espacio->precioBicicletasMinuto }}  por minuto</h3>
 
         <hr class="estadia-linea">
 
@@ -25,6 +32,10 @@
         </ul>
 
         <hr class="estadia-linea">
+
+        <h3 style="margin-top:0px; font-size:1.1em;">Este espacio tiene capacidad para alojar: <br> <br>{{ $espacio->cantAutos }} autos</h3>
+        <h3 style="margin-top:0px; font-size:1.1em;"> {{ $espacio->cantMotos }}  motos</h3>
+        <h3 style="margin-top:0px; font-size:1.1em;"> {{ $espacio->cantBicicletas }}  bicicletas</h3>
 
         <p>{{ $espacio->infopublica }}</p>
 
@@ -36,61 +47,141 @@
 
         <hr class="estadia-linea">
 
-        <div class="form-estadia" id="sticky-form"style="padding: 10px;">
-          <form class="search-espacios-form" action="{{ route('show.search')}}" method="get">
-            <div class="" id="dir_y_vehiculo">
-              <input type="search" name="search-espacios-input-direccion" placeholder="¿Dónde querés estacionar?">
-              <select name="search-espacios-vehiculo">
-                <option value="">Vehículo</option>
-                <option value="Auto">Auto</option>
-                <option value="Moto">Moto</option>
-                <option value="Bicicleta">Bicicleta</option>
-              </select>
-            </div>
-            <div class="search-horario">
-              <h5>Llegada</h5>
-              <input type="date" name="" value="">
-              <select name="search-espacios-hora-comienzo" class="search-espacios-hora">
-                <option value="">00</option>
-                <option value="">01</option>
-                <option value="">02</option>
-              </select>
-              <select name="search-espacios-minuto-comienzo" class="search-espacios-minuto">
-                <option value="">00</option>
-                <option value="">05</option>
-                <option value="">10</option>
-              </select>
-            </div>
-            <div class="search-horario">
-              <h5>Partida</h5>
-              <input type="date" name="" value="">
-              <select name="search-espacios-hora-fin" class="search-espacios-hora">
-                <option value="">00</option>
-                <option value="">01</option>
-                <option value="">02</option>
-              </select>
-              <select name="search-espacios-hora-fin" class="search-espacios-minuto">
-                <option value="">00</option>
-                <option value="">05</option>
-                <option value="">10</option>
-              </select>
-            </div>
-            <button type="submit" name="search-espacios-submit"><i class="fa fa-search"></i></button>
-            <input type="submit" name="BUSCAR" value="BUSCAR">
-          </form>
-        </div>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <br>
-
-        <div>
-
-        </div>
-
       </section>
 
+      <div class="form-generico" id="espacio-form">
+
+        <h3 style="margin-left:2px">{{ $espacio->tipoEspacio }}</h3>
+        <h4 style="margin-top:0px; font-size:1.1em;">Estadía con Auto $ {{ $espacio->precioAutosMinuto }}  por minuto</h4>
+
+        <form class="search-espacios-form" action="{{ route('show.search')}}" method="get">
+          <div class="" id="dir_y_vehiculo">
+            <input type="search" name="search-espacios-input-direccion" placeholder="¿Dónde querés estacionar?">
+            <select name="search-espacios-vehiculo">
+              <option value="Auto" selected>Auto</option>
+              <option value="Moto">Moto</option>
+              <option value="Bicicleta">Bicicleta</option>
+            </select>
+          </div>
+          <div class="search-horario">
+            <h5>Llegada</h5>
+            <input type="date" name="search-espacios-dia-comienzo" value="">
+            <select name="search-espacios-hora-comienzo" class="search-espacios-hora">
+              @for ($i=0; $i < 24; $i++)
+                @if ($i<10)
+                  <option value={{ $i }}>0{{ $i }}</option>
+                @else
+                  <option value={{ $i }}>{{ $i }}</option>
+                @endif
+              @endfor
+            </select>
+            <select name="search-espacios-minuto-comienzo" class="search-espacios-minuto">
+              @for ($i=0; $i < 60; $i+=5)
+                @if ($i<10)
+                  <option value={{ $i }}>0{{ $i }}</option>
+                @else
+                  <option value={{ $i }}>{{ $i }}</option>
+                @endif
+              @endfor
+            </select>
+          </div>
+          <div class="search-horario">
+            <h5>Partida</h5>
+            <input type="date" name="search-espacios-dia-fin" value="">
+            <select name="search-espacios-hora-fin" class="search-espacios-hora">
+              @for ($i=0; $i < 24; $i++)
+                @if ($i<10)
+                  <option value={{ $i }}>0{{ $i }}</option>
+                @else
+                  <option value={{ $i }}>{{ $i }}</option>
+                @endif
+              @endfor
+            </select>
+            <select name="search-espacios-minuto-fin" class="search-espacios-minuto">
+              @for ($i=0; $i < 60; $i+=5)
+                @if ($i<10)
+                  <option value={{ $i }}>0{{ $i }}</option>
+                @else
+                  <option value={{ $i }}>{{ $i }}</option>
+                @endif
+              @endfor
+            </select>
+          </div>
+          <button type="submit" name="search-espacios-submit"><i class="fa fa-search"></i></button>
+          <input type="submit" name="BUSCAR" value="BUSCAR">
+        </form>
+      </div>
+
+
+      <form method="POST" action="" style="display:inline;" onsubmit="confirm('ESTA POR RESERVAR ESTA COCHERA')">
+        {{ csrf_field() }}
+        <button id="reservar-cochera" type="submit" class="btn btn-success">RESERVAR ESTA COCHERA</button>
+      </form>
+
+      <div class="clear"></div>
+
+      <div class="">
+        <h2 style="margin-left:15px">El Barrio</h2>
+
+        <div id="map"></div>
+
+      </div>
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="js/menu.js"></script>
+  <script type="text/javascript">
+    var mymap = L.map('map').setView([-34.64, -58.38], 14);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1Ijoiam9hcGFub3MiLCJhIjoiY2pha2Z2eG1zMmlrNTMzcno2OHQ0b3VvYiJ9.jgj5HdcO2n9VZJpuSn4_wA'
+    }).addTo(mymap);
+
+    var marker = L.marker([-34.6211, -58.38151]).addTo(mymap);
+
+
+    marker.bindPopup("<h5>$ 80.00 ARS x hora</h5>").openPopup();
+
+
+    var popup = L.popup()
+      .setLatLng([-34.6111, -58.38151])
+      .setContent("Soy Digital House!")
+      .openOn(mymap);
+
+    function onMapClick(event) {
+      alert("Clickeaste el mapa en " + event.latlng);
+    }
+    mymap.on('click', onMapClick);
+    mymap.off('click', onMapClick);
+
+    var popup2 = L.popup()
+    function onMapClick2(e) {
+      popup2
+        .setLatLng(e.latlng)
+        .setContent("Clickeaste el mapa en " + e.latlng.toString())
+        .openOn(mymap)
+    }
+    // mymap.on('click', onMapClick2)
+
+  </script>
+
+  <script>
+    window.onscroll = function(){formSticky()};
+
+    var header = document.getElementById("espacio-form");
+    // var sticky = header.offsetTop;
+
+    function formSticky() {
+      if (window.pageYOffset >= 550) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+  </script>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,10 @@
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+     {{-- MAPA  --}}
+     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ==" crossorigin=""/>
+     <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js" integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log==" crossorigin=""></script>
+
       <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
