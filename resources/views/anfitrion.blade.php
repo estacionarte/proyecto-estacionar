bb83fb1d778e6d2c7d3af76c0c19a15da867e1ac
soylatrufa <marianoalvareztt@gmail.com>
2018-01-23 12:57:58 -0300

    trabaje en el master sin querer jojo

--- resources/views/anfitrion.blade.php
+++ resources/views/anfitrion.blade.php
@@ -27,7 +27,7 @@
 
     <div class="pasos-anfitrion-container">
       <h1>¿Cómo ser anfitrión en Estacionapp?</h1>
-      <article class="">
+      <article class="ser-anfitrion">
         <h2>Crea un espacio</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequ</p>
         <img src="" alt="">
