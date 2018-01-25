bca0442b471cb41542729602967c08143838e9db
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2018-01-25 13:06:02 -0300

    mejoras en alquiler. se puede alquilar desde profile

---
+++ public/js/modal-alquilar.js
@@ -0,0 +1,197 @@
+// Popup de botón Alquilar
+// Guardo variables a usar
+var modal = [];
+var btn = [];
+var close = [];
+var modalactivo;
+// Variables para validaciones
+var diacomienzo = [];
+var horacomienzo = [];
+var minutocomienzo = [];
+var fechacomienzo = [];
+var diafin = [];
+var horafin = [];
+var minutofin = [];
+var fechafin = [];
+var espacioid;
+var comienzo;
+var fin;
+var divNoDisponible = [];
+var btnsubmit = [];
+// Variables para precios
+var precio = [];
+var descuento = [];
+var total = [];
+
+// Función para actualizar fechas
+var actualizarReserva = function(){
+  espacioid = parseInt(modalactivo.closest("article").id);
+
+  fechacomienzo[espacioid] = new Date(diacomienzo[espacioid].value + " " + horacomienzo[espacioid].options[horacomienzo[espacioid].selectedIndex].value + ":" + minutocomienzo[espacioid].options[minutocomienzo[espacioid].selectedIndex].value);
+
+  fechafin[espacioid] = new Date(diafin[espacioid].value + " " + horafin[espacioid].options[horafin[espacioid].selectedIndex].value + ":" + minutofin[espacioid].options[minutofin[espacioid].selectedIndex].value);
+
+}
+
+// Función para convertir fechas a formato compatible con DateTime de php
+function convertirFechas(){
+  // Obtengo id del espacio que tengo que buscar
+  espacioid = parseInt(modalactivo.closest("article").id);
+  // Guardo fechas en el formato que las lee php
+  comienzo = fechacomienzo[espacioid].getFullYear() + '-' + (fechacomienzo[espacioid].getMonth()+1) + '-' + fechacomienzo[espacioid].getDate() + ' ' + fechacomienzo[espacioid].getHours() + ':' + fechacomienzo[espacioid].getMinutes();
+  fin = fechafin[espacioid].getFullYear() + '-' + (fechafin[espacioid].getMonth()+1) + '-' + fechafin[espacioid].getDate() + ' ' + fechafin[espacioid].getHours() + ':' + fechafin[espacioid].getMinutes();
+}
+
+// Actualizar precios ante cambios en horarios
+// Creo función que pide datos a la db
+
+var actualizarValores = function actualizar(){
+  // Actualizo valores de fechas y las convierto
+  actualizarReserva();
+  convertirFechas();
+
+  // Ajax call usando jquery
+  jQuery(function($){
+    $.ajax({
+      url: '/alquilar/detallealquiler/{id}/{horariollegada}/{horariopartida}',
+      // Paso headers con csrftoken por ser post
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      type: 'POST',
+      // Paso parámetros para función
+      data: {id:espacioid,horariollegada:comienzo,horariopartida:fin},
+      success: function(resultado) {
+        precio[espacioid].textContent = resultado.precio;
+        descuento[espacioid].textContent = resultado.descuento;
+        total[espacioid].textContent = resultado.total;
+      }
+    });
+  });
+
+  // Hago check de disponibilidad
+  chequearDisponibilidad();
+}
+
+// Popup
+for (var i = 0; i < espacios.length; i++) {
+  modal[i] = document.getElementsByClassName("modalAlquilar")[i];
+  btn[i] = document.getElementsByClassName("mejor-espacio-boton-alquilar")[i];
+  close[i] = document.getElementsByClassName("alquilar-close")[i];
+
+  id = parseInt(modal[i].id.slice(5));
+  modal[id] = modal[i];
+
+  // Abrir modal al apretar botón
+  btn[i].onclick = function() {
+    var modal = document.getElementById('modal' + this.id.slice(3));
+    window.modalactivo = modal;
+    modal.style.display = "block";
+  }
+
+  // Cerrar con cruz
+  close[i].onclick = function() {
+    var modal = document.getElementById('modal' + this.id.slice(3));
+    modal.style.display = "none";
+  }
+
+  // Cerrar al hacer click fuera de la caja
+  window.addEventListener('click', function(event) {
+    if (event.target == window.modalactivo) {
+      window.modalactivo.style.display = "none";
+    }
+  });
+
+  // Validaciones para form de alquiler
+  // No se puede elegir fecha anterior a la actual
+  diacomienzo[id] = document.querySelectorAll("input[name='alquiler-dia-comienzo']")[i];
+  diafin[id] = document.querySelectorAll("input[name='alquiler-dia-fin']")[i];
+
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //Enero es 0
+  var yyyy = today.getFullYear();
+
+  if(dd<10){
+    dd='0'+dd
+  }
+  if(mm<10){
+    mm='0'+mm
+  }
+
+  today = yyyy+'-'+mm+'-'+dd;
+  diacomienzo[id].setAttribute("min", today);
+  diafin[id].setAttribute("min", today);
+
+  // Asigno a variables los elementos a validar
+  horacomienzo[id] = document.querySelectorAll("select[name='alquiler-hora-comienzo']")[i];
+  minutocomienzo[id] = document.querySelectorAll("select[name='alquiler-minuto-comienzo']")[i];
+  horafin[id] = document.querySelectorAll("select[name='alquiler-hora-fin']")[i];
+  minutofin[id] = document.querySelectorAll("select[name='alquiler-minuto-fin']")[i];
+
+  // Guardo fechas introducidas
+  fechacomienzo[id] = new Date(diacomienzo[id].value + " " + horacomienzo[id].options[horacomienzo[id].selectedIndex].value + ":" + minutocomienzo[id].options[minutocomienzo[id].selectedIndex].value);
+
+  fechafin[id] = new Date(diafin[id].value + " " + horafin[id].options[horafin[id].selectedIndex].value + ":" + minutofin[id].options[minutofin[id].selectedIndex].value);
+
+  // Capturo elementos de precios a cambiar
+  precio[id] = document.querySelectorAll("span[precio='si']")[i];
+  descuento[id] = document.querySelectorAll("span[descuento='si']")[i];
+  total[id] = document.querySelectorAll("span[total='si']")[i];
+
+  // Agrego la función a los elementos
+  diacomienzo[id].addEventListener('change',actualizarValores);
+  horacomienzo[id].addEventListener('change',actualizarValores);
+  minutocomienzo[id].addEventListener('change',actualizarValores);
+  diafin[id].addEventListener('change',actualizarValores);
+  horafin[id].addEventListener('change',actualizarValores);
+  minutofin[id].addEventListener('change',actualizarValores);
+
+  // Capturo divs de no disponible y botones de submit
+  divNoDisponible[id] = document.querySelectorAll(".div-nodisponible")[i];
+  btnsubmit[id] = document.querySelectorAll("article input[type='submit']")[i];
+}
+
+// Función para chequear disponibilidad
+function chequearDisponibilidad(){
+  // Ajax call usando jquery
+  // Paso los parámetros que ya conseguí de las funciones ejecutadas anteriormente
+  jQuery(function($){
+    $.ajax({
+      url: '/alquilar/disponible/{id}/{horariollegada}/{horariopartida}',
+      // Paso headers con csrftoken por ser post
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      type: 'POST',
+      // Paso parámetros para función
+      data: {id:espacioid,horariollegada:comienzo,horariopartida:fin},
+      success: function(resultado) {
+        // Si el resultado es false (no disponible), muestro div informando esto y deshabilito botón de submit para que no mande el form
+        if (!resultado.disponibleTodo) {
+          divNoDisponible[espacioid].style.display = 'block';
+          btnsubmit[espacioid].disabled = true;
+        } else {
+          divNoDisponible[espacioid].style.display = 'none';
+          btnsubmit[espacioid].disabled = false;
+        }
+    }
+    });
+  });
+}
+
+// Creo función para validar datos cuando aprieto submit
+function validarForm(){
+  var ahora = new Date();
+  // Actualizo valores previo a verificaciones
+  actualizarReserva();
+
+  if (fechacomienzo[espacioid] <= ahora) {
+    alert("El horario a buscar debe ser mayor a la hora actual");
+    return false;
+  }
+
+  if (fechafin[espacioid] <= fechacomienzo[espacioid]) {
+    alert("La fecha de partida no puede ser anterior a la de llegada");
+    return false;
+  }
+
+  // Retorno false para evitar el envío. Lo que retorna true es la función chequearDisponibilidad
+  return true;
+}
---
+++ resources/views/_modal-alquilar.blade.php
@@ -0,0 +1,141 @@
+<div id="modal{{ $espacio->id }}" class="modalAlquilar">
+  <div class="modalAlquilar-content">
+    <span class="alquilar-close" id="btn{{ $espacio->id }}">&times;</span>
+    <h2>Reservar espacio</h2>
+    <h3>Nombre Espacio por Usuario</h3>
+
+    <hr>
+
+    @auth
+      <div class="form-generico">
+
+        <form action="{{ route('alquilar', $espacio->id) }}" method="post" onsubmit="return validarForm();">
+          {{ csrf_field() }}
+
+          <div class="modalAlquilar-form-div">
+
+            <label>Vehículo</label>
+            <select class="" name="vehiculo">
+
+              @forelse (Auth::user()->vehiculos as $vehiculo)
+                <option value="{{$vehiculo->id}}">{{$vehiculo->datos()}}</option>
+              @empty
+                <option value="">Cargar vehículo</option>
+              @endforelse
+            </select>
+
+          </div>
+
+          <div class="modalAlquilar-form-div div-nodisponible" style="font-size:1.2em; color: #990606; display:none;">
+            <p>Espacio no disponible en este horario</p>
+          </div>
+
+          <div class="modalAlquilar-form-div">
+
+            <label>Fecha y Hora</label>
+
+            <div class="modalAlquilar-form-div-div">
+              <div class="modalAlquilar-form-horarios">
+                <input type="date" name="alquiler-dia-comienzo" value="{{ $fechallegada->format('Y-m-d') }}">
+                <select name="alquiler-hora-comienzo" class="search-espacios-hora">
+                  @for ($i=0; $i < 24; $i++)
+                    @if ($i<10)
+                      <option value={{ $i }} {{ $i == $fechallegada->format('H') ? 'selected' : '' }}>0{{ $i }}</option>
+                    @else
+                      <option value={{ $i }} {{ $i == $fechallegada->format('H') ? 'selected' : '' }}>{{ $i }}</option>
+                    @endif
+                  @endfor
+                </select>
+                <span>:</span>
+                <select name="alquiler-minuto-comienzo" class="search-espacios-minuto">
+                  @for ($i=0; $i < 60; $i+=5)
+                    @if ($i<10)
+                      <option value={{ $i }} {{ $i == $fechallegada->format('i') ? 'selected' : '' }}>0{{ $i }}</option>
+                    @else
+                      <option value={{ $i }} {{ $i == $fechallegada->format('i') ? 'selected' : '' }}>{{ $i }}</option>
+                    @endif
+                  @endfor
+                </select>
+              </div>
+
+              <span style="font-size:1.4em; line-height: 47px; vertical-align:text-bottom;">&#10140;</span>
+
+              <div class="modalAlquilar-form-horarios">
+                <input type="date" name="alquiler-dia-fin" value="{{ $fechapartida->format('Y-m-d') }}">
+                <select name="alquiler-hora-fin" class="search-espacios-hora">
+                  @for ($i=0; $i < 24; $i++)
+                    @if ($i<10)
+                      <option value={{ $i }} {{ $i == $fechapartida->format('H') ? 'selected' : '' }}>0{{ $i }}</option>
+                    @else
+                      <option value={{ $i }} {{ $i == $fechapartida->format('H') ? 'selected' : '' }}>{{ $i }}</option>
+                    @endif
+                  @endfor
+                </select>
+                <span>:</span>
+                <select name="alquiler-minuto-fin" class="search-espacios-minuto">
+                  @for ($i=0; $i < 60; $i+=5)
+                    @if ($i<10)
+                      <option value={{ $i }} {{ $i == $fechapartida->format('i') ? 'selected' : '' }}>0{{ $i }}</option>
+                    @else
+                      <option value={{ $i }} {{ $i == $fechapartida->format('i') ? 'selected' : '' }}>{{ $i }}</option>
+                    @endif
+                  @endfor
+                </select>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="modalAlquilar-form-div">
+
+            <div class="modalAlquilar-form-lineadetalle">
+              <div class="lineadetalle-motivo">
+                <span>Precio</span>
+                <span>|?|</span>
+              </div>
+              <div class="lineadetalle-precio">
+                <span>$</span><span precio='si'>{{ $espacio->precio($fechallegada, $fechapartida) }}</span>
+              </div>
+            </div>
+
+            <div class="modalAlquilar-form-lineadetalle lineadetalle-descuento">
+              <div class="lineadetalle-motivo">
+                <span>Descuento</span>
+                <span>|?|</span>
+              </div>
+              <div class="lineadetalle-precio">
+                <span>-$</span><span descuento='si'>{{ $espacio->descuento($fechallegada, $fechapartida) }}</span>
+              </div>
+            </div>
+
+            <div class="modalAlquilar-form-lineadetalle lineadetalle-total">
+              <div class="lineadetalle-motivo">
+                <span>TOTAL</span>
+              </div>
+              <div class="lineadetalle-precio">
+                <span>$</span><span total='si'>{{ $espacio->precioFinal($fechallegada, $fechapartida) }}</span>
+              </div>
+            </div>
+
+          </div>
+
+          <input type="submit" name="reservar" value="RESERVAR">
+
+        </form>
+
+      </div>
+
+      <p>Todavía no se hará ningún cobro</p>
+
+    @endauth
+
+    @guest
+
+      <div class="">
+        <h4 style="text-align:center; margin: 15px auto;">Debés <a href="{{ route('login') }}">iniciar sesión</a> antes de alquilar un espacio</h4>
+      </div>
+
+    @endguest
+
+  </div>
+</div>
