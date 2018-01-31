594989706c2165f8cc2aaebfbfbd0197899c7108
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2018-01-31 19:33:52 -0300

    correcion en controller de envio de mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\mail\EmailContact;
+use App\Mail\EmailContact;
 
 class ContactController extends Controller
 {
