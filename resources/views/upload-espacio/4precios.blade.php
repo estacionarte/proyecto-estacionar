d46ca3f28a5841a729bebff5169979b66485ecb3
Joaquin Paños <joaquinpanos@Joaquins-MacBook-Pro.local>
2018-05-28 21:13:10 -0300

    avances

--- resources/views/upload-espacio/4precios.blade.php
+++ resources/views/upload-espacio/4precios.blade.php
@@ -28,7 +28,7 @@
             {{ method_field('PUT') }}
             {{ csrf_field() }}
 
-            <label for="" class="upload-label-titulo">¿Cuál va a ser el precio por minuto de tu espacio?</label>
+            <label for="" class="upload-label-titulo">¿Cuál va a ser el precio por hora de tu espacio?</label>
 
             <div class="upload-div-precio">
               <label for="" class="upload-label-precioPorMinutoSigno">$</label>
@@ -62,7 +62,7 @@
 
             <div class="upload-div-precio">
               {{-- <input type="number" placeholder="35" name="descuentoPorMinutoSeisHoras" class="upload-input-descuentoPorMinuto" value="{{ old('descuentoPorMinutoSeisHoras', $espacio->descuentos()->where('hora',6)->first() ? $espacio->getDescuento(6)*100 : '') }}"> --}}
-              <select name="descuentoPorMinutoSeisHorasHora" class="upload-input-descuentoPorMinuto" required>
+              <select name="descuentoPorMinutoSeisHoras" class="upload-input-descuentoPorMinuto" required>
                 <option value="" selected>Descuento</option>
                 <option value=0.2>20</option>
                 <option value=0.3>30</option>
@@ -91,10 +91,23 @@
             </div>
             <p class="upload-p-descuentoPorMinuto">Precio por día con descuento: $ <b id="precioDia">324</b></p>
 
+            <div class="">
+              <h3>Calculadora</h3>
+
+              <h4>Duración Alquiler</h4>
+              <label for="">Días:</label><input type="text" name="" value=0 id="dias-calculadora">
+              <label for="">Horas:</label><input type="text" name="" value=3 id="horas-calculadora">
+              <label for="">Minutos:</label><input type="text" name="" value=30 id="minutos-calculadora">
+
+              <p>Por dejar el auto <span id="dias-resultado">0</span> dias, <span id="dias-resultado">3</span> horas y <span id="dias-resultado">30</span> minutos recibirás $<span id="ganancia-resultado"></span></p>
+
+            </div>
+
             <input type="submit" name="boton-volver" value="&#8249; Volver" class="upload-button-volver" formaction="{{ route('upload.espacio.3', $espacio) }}" formmethod="get">
             <input type="submit" name="boton-submit" value="SIGUIENTE" class="upload-button-submit">
 
           </form>
+
 
           <section class="upload-section-help">
             <div class="upload-section-div-help">
